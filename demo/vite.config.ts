fd31390d491ce96ddf2b35cd168f1fe6f9b412c0
Marko Bolliger <bolligermarko@gmail.com>
2023-09-25 18:21:51 +0200

    new

--- demo/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import postcssConfig from './postcss.config';
-import { viteSingleFile } from 'vite-plugin-singlefile';
-
-export default defineConfig({
-  plugins: [viteSingleFile()],
-});
