ad6d2297defe742bbd4b4064ceff46c5e549a666
Marko Bolliger <bolligermarko@gmail.com>
2023-09-25 17:10:23 +0200

    push

--- src/generatePDF.ts
+++ src/generatePDF.ts
@@ -1,7 +1,4 @@
 import puppeteer from 'puppeteer';
-import { readFile } from 'node:fs/promises';
-import inlinCss from 'inline-css';
-const inlineCSS = (html) => {};
 
 export const generate = async (html: string) => {
   const browser = await puppeteer.launch();
@@ -10,13 +7,15 @@ export const generate = async (html: string) => {
   await page.setContent(html);
   await browser.close();
 
-  return page.pdf({
-    format: 'A4',
-    margin: {
-      top: '20px',
-      left: '20px',
-      right: '20px',
-      bottom: '20px',
-    },
-  });
+  page
+    .pdf({
+      format: 'A4',
+      margin: {
+        top: '20px',
+        left: '20px',
+        right: '20px',
+        bottom: '20px',
+      },
+    })
+    .then((res) => {});
 };
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,25 @@ import { Hono } from 'hono';
 import { prettyJSON } from 'hono/pretty-json';
 import { serveStatic } from 'hono/bun';
 import { timing, startTime, endTime } from 'hono/timing';
+import fs from 'node:fs/promises';
 const app = new Hono();
 app.use('*', timing());
 app.use('*', prettyJSON());
 app.use('/public/*', serveStatic({ root: './' }));
 app.get('/', async (c) => {
   startTime(c, 'generate_pdf');
-
+  c.header('Content-Type', 'application/pdf');
+  const a = await new Promise((resolve, recjet) => {
+    setTimeout(() => {
+      resolve('nice');
+    }, 3000);
+  });
+  const file = await fs.readFile('example.html');
+  console.log(file);
   endTime(c, 'generate_pdf');
-  return c.jsonT({ ok: true });
+  return c.jsonT({
+    bla: a,
+  });
 });
-console.log('wtf');
+
 export default app;
